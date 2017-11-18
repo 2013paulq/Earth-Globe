d296f72c1088c7161bcb4668afc514857d3d52b4
Paul <quastie@hotmail.com>
2017-11-18 20:44:23 +0000

    faster

--- test2/globe.js
+++ test2/globe.js
@@ -450,7 +450,7 @@ DAT.Globe = function(container, opts){
         // Earth rotation speed
 		// Milliseconds in a day - 86400000
 		if (timestamp) {
-			target.x -= (timestamp - previousTimestamp)*6/86400;
+			target.x -= (timestamp - previousTimestamp)*6/75000;
 			previousTimestamp = timestamp;
 		}
 
